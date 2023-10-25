eb84563b2337c2de5aea2c5b433a8411cc0c60f5
vidhangithub <vidhan.amc@gmail.com>
2023-10-26 00:17:29 +0100

    First commit

---
+++ src/main/java/com/vidhan/graphql/rpcegraphql/RpceGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.vidhan.graphql.rpcegraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpceGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpceGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidhan/graphql/rpcegraphql/RpceGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhan.graphql.rpcegraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpceGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
